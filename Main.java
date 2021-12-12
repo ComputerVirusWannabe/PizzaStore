c914234e772cfaeee87fb810a7d57ee7222103fb
ComputerVirusWannabe <94106778+ComputerVirusWannabe@users.noreply.github.com>
2021-12-12 11:55:31 +0700

    2nd factory commit

--- Main.java
+++ Main.java
@@ -1,121 +1,93 @@
+// Very first Pizza Factory class
+
 class Main {
 	public static void main(String[] args) {
-	    PizzaStore USstore = new AmericanPizzaStore();
-        USstore.createPizza("beef");
-        
+	    PizzaStore pizza_store = new VNPizzaStore();
+        pizza_store.order("beef");
 	}
 }
 
 abstract class Pizza {
-    abstract public void prepare();
-    abstract public void bake();
+ 
 }
-
-class VegiePizza extends Pizza {
-    public void prepare () {
-        
-    }
-    public void bake() {
-        
-    }
+ 
+class VNVegiePizza extends Pizza {
+ 
 }
-
-class BeefPizza extends Pizza {
-    public void prepare () {
-        
-    }
-    public void bake() {
-        
-    }
+ 
+class VNBeefPizza extends Pizza {
+ 
 }
-
-
-
-class ChickenPizza extends Pizza {
-    public void prepare () {
-        
-    }
-    public void bake() {
-        
-    }
+ 
+class USVegiePizza extends Pizza {
+ 
 }
-
-
-/*
-class PizzaFactory {
-    public Pizza createPizza(String pizzaName) {
-        Pizza pizza = null;
-        if(pizzaName == "vegie") {
-            pizza = new VegiePizza();
-            System.out.println("Pizza for the vegans...");
-        } else if (pizzaName == "beef") {
-            pizza = new BeefPizza();
-            System.out.println("Beef pizza comin' right up...");
-        } else if (pizzaName == "chicken"){
-            pizza = new ChickenPizza();
-            System.out.println("Here comes the chicken pizza");
-        } else {
-            System.out.println("We don't serve such pizzas here valued customer..");
-            //return null;
-        }
-        return pizza;
-    }
+ 
+ 
+class USBeefPizza extends Pizza {
+ 
 }
-
-*/
 
 abstract class PizzaStore {
-    /*
-     Pizza order(String pizzaType) {
-        return createPizza(pizzaType);
-    }
+    public abstract Pizza createPizza(String taste);
+    /* 
+    {
+        Pizza pizza = null;
+        if(style == "american" && taste == "vegie") {
+            pizza = new USVegiePizza();
+            System.out.println("Pizza for the vegans...");
+        } else if (style == "american" && taste == "beef") {
+            pizza = new USBeefPizza();
+            System.out.println("Beefy bois are always the tastiest!");
+        } else if (style == "vietnamese" && taste == "vegan"){
+            pizza = new VNVegiePizza();
+            System.out.println("Nướng cái pida cho mấy thằng ăn chay!");
+        } else if (style == "vietnamese" && taste == "beef") {
+            pizza = new VNBeefPizza();
+            System.out.println("Nướng cho tụi nó vài miếng thịt bò rồi biểu nó cút xéo.");
+
+        } else {
+            System.out.println("An error has occured. If you have a grievance, just sue us. We dare you.");
+            //return null;
+        }
+        return pizza;
+    } 
     */
     
-    public abstract Pizza createPizza(String pizzaName);
-
+    Pizza order(String taste) {
+        return createPizza(taste);
+    }
 }
 
 
-class VietnamesePizzaStore extends PizzaStore {
-
-    public Pizza createPizza(String pizzaName) {
+class VNPizzaStore extends PizzaStore {
+    public Pizza createPizza (String taste) {
         Pizza pizza = null;
-        if(pizzaName == "vegie") {
-            pizza = new VegiePizza();
-            System.out.println("Pida cho mấy thằng ăn chay");
-        } else if (pizzaName == "beef") {
-            pizza = new BeefPizza();
-            System.out.println("Pida con bò..");
-        } else if (pizzaName == "chicken"){
-            pizza = new ChickenPizza();
-            System.out.println("Giết mấy con gà rồi cho tụi nó");
+        if (taste == "beef") {
+            pizza = new VNBeefPizza();
+            System.out.println("Cho tụi nó vài miếng thịt bò rồi hốt xác nó.");
+        } else if (taste == "vegie") {
+            pizza = new VNVegiePizza();
+            System.out.println("Nướng cái pida cho mấy thằng ăn chay!");
         } else {
-            System.out.println("Cút khổi chỗ này ngay! Ỡ đay không có Pida loại đó!");
-            //return null;
-        }
-        return pizza;
-    }    
-    
-}
-
-class AmericanPizzaStore extends PizzaStore {
-
-    public Pizza createPizza(String pizzaName) {
-        Pizza pizza = null;
-        if(pizzaName == "vegie") {
-            pizza = new VegiePizza();
-            System.out.println("Pizza for the vegans...");
-        } else if (pizzaName == "beef") {
-            pizza = new BeefPizza();
-            System.out.println("Beef pizza comin' right up...");
-        } else if (pizzaName == "chicken"){
-            pizza = new ChickenPizza();
-            System.out.println("Here comes the chicken pizza");
-        } else {
-            System.out.println("We don't serve such pizzas here valued customer..");
-            //return null;
+            System.out.println("Ở đây không có pida loại đó! Xéo!");
         }
         return pizza;
     }
 }
 
+class USPizzaStore extends PizzaStore {
+    public Pizza createPizza (String taste) {
+        Pizza pizza = null;
+        if (taste == "beef") {
+            pizza = new USBeefPizza();
+            System.out.println("Beefy bois are always the tastiest!");
+        } else if (taste == "vegie") {
+            pizza = new USVegiePizza();
+            System.out.println("Pizza for the vegans...");
+        } else {
+            System.out.println("An error has occured. If you have a grievance, just sue us. We dare you.");
+        }
+    return pizza;
+    }
+}
